fb1df0d68217d59ab3be1ec3bbab778e273b0920
Sung Yi-Tzu <ziyisong18@gmail.com>
2025-06-20 17:35:51 +0800

    feat: configure next.config.mjs to allow next/image loading images from placehold.co

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,14 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+    ],
+  },
   async headers() {
     return [
       {
